e77437f4bbee68e1ce90aa6caefab1a9141a1998
BARATH KUMAR B <vm.b.barathkumar@gmail.com>
2020-11-16 22:05:44 +0530

    Update Arthmetic Operators (sense hat ).py

--- Python/Arthmetic Operators (sense hat ).py
+++ Python/Arthmetic Operators (sense hat ).py
@@ -1,1 +1,83 @@
+from sense_hat import SenseHat
+import time
 
+s = SenseHat()
+s.low_light = True
+
+green = (0, 255, 0)
+yellow = (255, 255, 0)
+blue = (0, 0, 255)
+red = (255, 0, 0)
+white = (255,255,255)
+nothing = (0,0,0)
+pink = (255,105, 180)
+
+def trinket_logo():
+    G = green
+    Y = yellow
+    B = blue
+    O = nothing
+    logo = [
+   O, O, O, O, O, O, G, G,
+   O, G, G, O, O, G, G, O,
+   O, G, G, O, G, G, O, O,
+   O, O, O, G, G, O, O, O,
+   O, O, G, G, O, O, O, O,
+   O, G, G, O, O, G, G, O,
+   G, G, O, O, O, G, G, O,
+   G, O, O, O, O, O, O, O,]
+    return logo
+
+def raspi_logo():
+    G = green
+    R = red
+    O = nothing
+    logo = [
+   O, O, O, O, O, O, O, O,
+   O, O, O, O, O, O, O, O,
+   O, O, O, O, O, O, O, O,
+   O, R, R, R, R, R, R, O,
+   O, R, R, R, R, R, R, O,
+   O, O, O, O, O, O, O, O,
+   O, O, O, O, O, O, O, O,
+   O, O, O, O, O, O, O, O,
+   ]
+    return logo
+
+def plus():
+    W = white
+    O = nothing
+    logo = [
+    O, O, O, O, O, O, O, O, 
+    O, O, O, W, W, O, O, O,
+    O, O, O, W, W, O, O, O, 
+    O, W, W, W, W, W, W, O,
+    O, W, W, W, W, W, W, O,
+    O, O, O, W, W, O, O, O,
+    O, O, O, W, W, O, O, O,
+    O, O, O, O, O, O, O, O,
+    ]
+    return logo
+
+def equals():
+    W = white
+    O = nothing
+    logo = [
+    O, O, O, O, O, O, O, O, 
+    O, W, W, W, W, W, W, O,
+    O, W, W, W, W, W, W, O,
+    O, O, O, O, O, O, O, O,
+    O, O, O, O, O, O, O, O,
+    O, W, W, W, W, W, W, O,
+    O, W, W, W, W, W, W, O,
+    O, O, O, O, O, O, O, O,
+    ]
+    return logo
+
+images = [trinket_logo, trinket_logo, plus, raspi_logo, raspi_logo, equals]
+count = 0
+
+while True: 
+    s.set_pixels(images[count % len(images)]())
+    time.sleep(.75)
+    count += 1
